602b3532c9b2da0feef2361fcc6ff327e30c0123
RiP3rQ <rafalpompa2000@gmail.com>
2022-10-22 14:33:58 +0200

    feat; post context setyp

---
+++ client/src/contexts/PostContext.js
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect, useMemo, useState } from "react"
+import { useParams } from "react-router-dom"
+import { useAsync } from "../hooks/useAsync"
+import { getPost } from "../services/posts"
+
+const Context = React.createContext()
+
+export function usePost() {
+    return useContext(Context)
+  }
+
+export function PostProvider({ children }) {
+
+    const { id } = useParams()
+    const { loading, error, value: post } = useAsync( () => getPost(id), [id])
+
+    return ( <Context.Provider value={{
+        post: { id, ...post },
+    }}>
+        {loading ? (
+        <h1>Loading</h1>
+      ) : error ? (
+        <h1 className="error-msg">{error}</h1>
+      ) : (
+        children
+      )}
+        
+        </Context.Provider>
+    )
+}
